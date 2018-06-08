176b8eb7d47c5bb82f3311a49f19aaefdebfbae7
117503445 <t117503445@gmail.com>
2018-06-08 09:02:36 +0800

    GoodByeChangHe

---
+++ HTRemotingServiceLibrary/Ftp/FtpDest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Remoting.Ftp
+{
+    public class FtpDest
+    {
+
+    }
+
+    public class FtpFileSystemInfo
+    {
+
+    }
+}
